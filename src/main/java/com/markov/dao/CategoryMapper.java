973a210eb689758f4feff03041979c26562053e3
1993HZC <hzc19930921@163.com>
2020-06-30 23:39:11 +0800

    准备分页的东西

--- src/main/java/com/markov/dao/CategoryMapper.java
+++ src/main/java/com/markov/dao/CategoryMapper.java
@@ -1,9 +1,11 @@
 package com.markov.dao;
 
 import com.markov.pojo.Category;
+import com.markov.util.Page;
 
 import java.util.List;
 
 public interface CategoryMapper {
     List<Category>list();
+
 }
--- src/main/java/com/markov/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/markov/service/impl/CategoryServiceImpl.java
@@ -1,16 +1,19 @@
 package com.markov.service.impl;
 
+import com.markov.dao.CategoryMapper;
 import com.markov.pojo.Category;
 import com.markov.service.ICategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements ICategoryService {
-
+    @Autowired
+    CategoryMapper categoryMapper;
     @Override
     public List<Category> getAllCategory() {
-        return null;
+        return categoryMapper.list();
     }
 }
