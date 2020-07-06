f22871b53610491aa06af08d6beb65a077d5760f
1993HZC <hzc19930921@163.com>
2020-07-06 21:36:04 +0800

    修复product页面点击点击下一页出错

--- src/main/java/com/markov/controller/ProductController.java
+++ src/main/java/com/markov/controller/ProductController.java
@@ -33,9 +33,18 @@ public class ProductController {
 
         int total= (int) new PageInfo<>(productList).getTotal();
         page.setTotal(total);
+
+        /*将这个&cid=这个参数存入到listProduct这个页面当中
+        *
+        * 从admin_category_list页面点击admin_product_list页面的时候admin_category_list会自带&cid=参数，所以page.setParam("&cid="+cid);可有可无是不会报错的
+        * 从admin_category_list页面自己点击第2页或者下一页，jsp代码就从当前页上拿到点击当前页是已经存好的&cid=参数，如果之前第一次进入到这个页面没有存这个&cid=,那么在当前页面点击同属当前jsp的
+        * 下一页就会报错
+         * */
+        page.setParam("&cid="+cid);
         model.addAttribute("page",page);
         model.addAttribute("ps",productList);
         model.addAttribute("c",category);
+        model.addAttribute("cid",cid);
         return "admin/listProduct";
     }
     @RequestMapping("admin_product_add")
